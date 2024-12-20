c9cc2d41216c7362b88f15434f4a3d9baa8c0c78
John Doe <johndoe@email.com>
2024-12-20 17:16:44 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About me</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        $(function(){
+            $("#header").load("header.php");
+            $("#footer").load("footer.php");
+        });
+    </script>
+</head>
+<body style="background-color:black;text-align:center;color:white">
+    <link rel="stylesheet" href="css/styles.css">
+    <div id="header"></div>
+    <!-- <center> Stefan Forstinger </center>k -->
+</body>
+</html>
